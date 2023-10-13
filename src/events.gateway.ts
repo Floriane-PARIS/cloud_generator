ecb84be8979f46c82ca9fbe9413b985fa1a8b78f
Hadil Ayari <hadilayari00@gmail.com>
2023-10-14 00:19:46 +0200

    cors update

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -7,7 +7,14 @@ import {
   } from '@nestjs/websockets';
   import { Server } from 'socket.io';
   
-  @WebSocketGateway() 
+  @WebSocketGateway({
+    cors: {
+      origin: "https://app-061c7eb9-4e4d-4bff-a3ba-ac5f184e2f25.cleverapps.io",
+      methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+      credentials: true,
+    },
+  })
+  
   export class EventsGateway implements OnGatewayConnection {
     @WebSocketServer()
     server: Server;
@@ -20,7 +27,7 @@ import {
   
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): WsResponse<string> {
-      console.log('Handling message', payload);
+        console.log('Handling message', payload);
       const { idSender, idReceiver, msg } = payload;
       this.server.emit('newMessage', `New message from ${idSender} to ${idReceiver}: ${msg}`);
       return { event: 'message', data: 'Message received!' };
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: process.env.REACT_APP,
+    origin: 'https://app-061c7eb9-4e4d-4bff-a3ba-ac5f184e2f25.cleverapps.io',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
   };
   app.enableCors(corsOptions);
 
