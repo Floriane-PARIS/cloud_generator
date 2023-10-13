951660e4dc251d6641591966f3e05839553b8e13
Hadil Ayari <hadilayari00@gmail.com>
2023-10-14 00:02:05 +0200

    websocket test

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -7,18 +7,20 @@ import {
   } from '@nestjs/websockets';
   import { Server } from 'socket.io';
   
-  @WebSocketGateway(8080)
+  @WebSocketGateway() 
   export class EventsGateway implements OnGatewayConnection {
     @WebSocketServer()
     server: Server;
   
     handleConnection(client: any, ...args: any[]) {
       console.log('New client connected');
+      console.log(args);
       client.emit('connection', 'Successfully connected to server');
     }
   
     @SubscribeMessage('message')
     handleMessage(client: any, payload: any): WsResponse<string> {
+      console.log('Handling message', payload);
       const { idSender, idReceiver, msg } = payload;
       this.server.emit('newMessage', `New message from ${idSender} to ${idReceiver}: ${msg}`);
       return { event: 'message', data: 'Message received!' };
