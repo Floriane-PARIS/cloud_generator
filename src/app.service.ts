5375578240dbf90f64bc743118ebc33a4f0aa79b
Sylcantor <guiblinnicolas@gmail.com>
2023-10-16 14:38:15 +0200

    fix get history message from a conversation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ObjectId } from 'mongodb';
 const { MongoClient } = require('mongodb');
 
 
@@ -14,34 +15,42 @@ export class AppService {
     return 'Hello World!';
   }
 
-  getDb(): string {
+  async getDb(): Promise<any> {
     // Replace the URI with your own MongoDB URI
-    const uri = "mongodb://uzqce0sg9aevrorkmrrv:gVbmOxI9Z2OAnXiSf1E@bwtohbemviuyaixfsi7c-mongodb.services.clever-cloud.com:2388/bwtohbemviuyaixfsi7c"
+    const uri = "mongodb://uroccssvevcnzmiglusv:ptizczpirzvz3psji1m9r3psdr48vm@bhogfnwfnzjnxwhnchfs-mongodb.services.clever-cloud.com:2120/bhogfnwfnzjnxwhnchfs"
 
     // Create a new MongoClient
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    async function run() {
-      try {
-        // Connect to the MongoDB server
-        await client.connect();
-    
-        // Access the database
-        const database = client.db("bwtohbemviuyaixfsi7c");
-    
-        // Access the collection
-        const collection = database.collection("messaging");
-        //"651c1695c8838005550b365a"
-        // Example query
-        const result = await collection.findOne({});
-    
-        console.log(result);
-      } finally {
-        // Close the connection when done
-        await client.close();
-      }
+    try {
+      // Connect to the MongoDB server
+      await client.connect();
+  
+      // Access the database
+      const database = client.db("bhogfnwfnzjnxwhnchfs");
+  
+      // Access the collection
+      const collection = database.collection("conversation");
+  
+      // Get the document with id 652924bdc5faf4a0ad9e9ab0
+      const id = "652924bdc5faf4a0ad9e9ab0";
+  
+      // convert id from string to ObjectId
+      const query = { _id: new ObjectId(id) };
+  
+      const result = await collection.findOne(query);
+  
+      // Log and return messages array
+      console.log(result.messages);
+      return result.messages;
+  
+    } catch (err) {
+      console.log(err);
+      return { error: err };
+    } finally {
+      // Close the connection when done
+      await client.close();
     }
-    run().catch(console.error);
-    return "";
+    
   }
 }
 
